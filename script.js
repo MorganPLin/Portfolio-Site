58f3e2a9ac4026391f2245e0825bdcbfdb569bd6
Morgan Lin <morganlin@morgans-mbp.attlocal.net>
2017-06-07 16:41:47 -0700

    added side-menu

---
+++ script.js
@@ -0,0 +1,12 @@
+// SIDE MENU
+
+function showMenu () {
+	document.getElementById("side-menu").style.visibility = "visible";
+	document.getElementById("side-menu-icon").style.visibility = "hidden";
+	console.log("hi")
+}
+
+function hideMenu () {
+	document.getElementById("side-menu").style.visibility = "hidden";
+	document.getElementById("side-menu-icon").style.visibility = "visible";
+}
